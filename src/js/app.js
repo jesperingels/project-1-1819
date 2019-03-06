79801d2582bc90f11e6b0cb282d5407924b3faca
jesperingels <jesper.ingels@gmail.com>
2019-03-06 13:28:26 +0100

    Update js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,5 @@
 import{ API } from"../node_modules/oba-wrapper/js/index.js";
 
-const api = new API({
-    key: "1e19898c87464e239192c8bfe422f280"
-});
-console.log(api);
-
 //Imagine the functions toJson, cleanJSON and
 //renderToDocument exist, and do what their
 //name says.
@@ -16,53 +11,92 @@ console.log(api);
 //         console.log(response);
 //     }
 // })();
+const scan = document.getElementById('scan');
+const head = document.querySelector('head');
+
+
+scan.addEventListener('click', () => {
+
+    //use css custom property?
+    scan.style.display = 'none';
+
+     function insertHTML() {
+        head.insertAdjacentHTML('afterbegin', '<script id="quagga-script" src="src/node_modules/quagga/dist/quagga.min.js"></script>');
+        // quagga.addEventListener('onload', scanner.init);
+
+         const quagga = document.querySelector('script[id="quagga-script"]');
+         console.log(quagga);
+
+         // debugger;
+
+         if (quagga) {
+             scanner.init();
+         } else {
+             console.log('script not loaded');
+         }
+    }
+
+    insertHTML();
+
+});
+
+const api = new API({
+    key: "1e19898c87464e239192c8bfe422f280"
+});
+console.log(api);
 
 const data = {
 
-    req: async (string) => {
-        const iterator = await api.createIterator(`search/${string}`);
-        
+    req: async (endpoint) => {
+        const iterator = await api.createIterator(`search/${endpoint}`);
+
         for await (const response of iterator) {
-            console.log(response);
+            console.log(response)
+            // document.getElementById('text-container').textContent = response.titles.short-title._text
         }
     }
 };
 
+const scanner = {
 
-if(navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function'){
-    Quagga.init({
-        inputStream : {
-            name : "Live",
-            type : "LiveStream",
-            target: document.querySelector('body')    // Or '#yourElement' (optional)
-        },
-        decoder : {
-            readers : ["ean_reader"]
+    init: () => {
+        console.log('init scanner');
+        if(navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function' && document.getElementById('quagga-script')) {
+            Quagga.init({
+                inputStream : {
+                    name : "Live",
+                    type : "LiveStream",
+                    target: document.querySelector('body')    // Or '#yourElement' (optional)
+                },
+                decoder : {
+                    readers : ["ean_reader"]
+                }
+            }, function(err) {
+                if (err) {
+                    console.log(err);
+                    return
+                }
+                console.log("Initialization finished. Ready to start");
+                Quagga.start();
+            });
+
+            Quagga.onDetected((res)=>{
+                console.log(res.codeResult.code);
+                data.req(res.codeResult.code);
+                Quagga.stop();
+            });
+
+            console.log(Quagga.onDetected.data);
+
+        } else{
+            console.log('err')
         }
-    }, function(err) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-    });
-    // document.querySelector('#finder').style.setProperty("display","none");
-    Quagga.onDetected((res)=>{
-        console.log(res.codeResult.code);
-        document.getElementById('text-container').textContent = res.codeResult.code;
-        data.req(res.codeResult.code);
-        Quagga.stop();
-    });
+    }
 
-    console.log(Quagga.onDetected.data);
-}else{
-    console.log('err')
-}
+};
 
-// const req = {
-//
-// };
+
+
 
 
 
